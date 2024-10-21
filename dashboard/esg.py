053b310763a2370a13f9cd97c7c7d15b3f19de76
Stefan Lazarevic <stefan.lazarevic@cm.com>
2024-10-21 18:44:05 +0200

    added avg scores for ESGs on the home page and updated some icons. Added filtering for ESG scores per stock and added a graph for ESG scores per industry

--- dashboard/esg.py
+++ dashboard/esg.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 
-st.subheader("Environment, Social, Governance Scores Analysis (ESG)")
+st.subheader("Environment, Social, Governance Scores Analysis (ESG)", divider=True)
 
 # Create a SQLAlchemy engine to connect to the PostgreSQL database
 engine = create_engine("postgresql://team13:team13@esg-stocks-db:5432/esg-stocks-database")
@@ -22,6 +21,7 @@ FROM stock AS st
 INNER JOIN esg_history AS esg ON st.ticker_symbol = esg.ticker_symbol
 INNER JOIN pricing_history AS ph ON ph.ticker_symbol = st.ticker_symbol
 GROUP BY st.ticker_symbol, st.name
+LIMIT 50
 """
 
 # Use pandas to read the data
@@ -29,27 +29,47 @@ df = pd.read_sql_query(query, engine)
 
 # Sort the DataFrame
 df_sorted = df.sort_values(by='max_esg_score', ascending=False)
+# Determine the maximum ESG score across all stocks
+max_esg_score = df_sorted[['max_env_score', 'max_social_score', 'max_governance_score']].max().max()
+
+# Add CSS to reduce the width of the selectbox
+st.markdown(
+    """
+    <style>
+    .stSelectbox [data-baseweb="select"] {
+        width: 20rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Let users select a stock
+stock_names = df_sorted['name'].unique()
+selected_stock = st.selectbox("Select a Stock", stock_names)
+
+# Filter the dataframe for the selected stock
+selected_df = df_sorted[df_sorted['name'] == selected_stock]
 
 # Create a bar chart using Plotly
 fig = go.Figure()
 
 fig.add_trace(go.Bar(
-    x=df['name'],
-    y=df['max_env_score'],
+    x=selected_df['name'],
+    y=selected_df['max_env_score'],
     name='Environment',
     marker_color='lightblue'
 ))
 
 fig.add_trace(go.Bar(
-    x=df['name'],
-    y=df['max_social_score'],
+    x=selected_df['name'],
+    y=selected_df['max_social_score'],
     name='Social',
     marker_color='lightgreen'
 ))
 
 fig.add_trace(go.Bar(
-    x=df['name'],
-    y=df['max_governance_score'],
+    x=selected_df['name'],
+    y=selected_df['max_governance_score'],
     name='Governance',
     marker_color='lightpink'
 ))
@@ -57,15 +77,83 @@ fig.add_trace(go.Bar(
 # Update layout
 fig.update_layout(
     barmode='group',
-    xaxis_tickangle=-45,
     xaxis_title='Stocks',
     yaxis_title='ESG Scores',
-    title='ESG Scores by Category',
+    yaxis=dict(
+        title='ESG Scores',
+        range=[0, max_esg_score + 100]  # Fixed y-axis range
+    ),
+    title=f'ESG Scores for {selected_stock}',
     legend_title_text='Categories',
-    width=1200,  # Set width
-    height=700,  # Set height
+    width=800,  # Adjust width
+    height=600,  # Adjust height
     legend=dict(yanchor="top", y=1.15, xanchor="left", x=1.05)
 )
 
 # Render the chart in Streamlit
 st.plotly_chart(fig)
+
+
+# Join all 3 tables and select certain columns
+query = """
+SELECT 
+st.industry, 
+CAST(MAX(esg.total_score) AS numeric) AS max_esg_score,
+CAST(MAX(esg.environment_score) AS numeric) AS max_env_score,
+CAST(MAX(esg.social_score) AS numeric) AS max_social_score,
+CAST(MAX(esg.governance_score) AS numeric) AS max_governance_score
+FROM stock AS st
+INNER JOIN esg_history AS esg ON st.ticker_symbol = esg.ticker_symbol
+GROUP BY st.industry
+LIMIT 10
+"""
+
+# Use pandas to read the data
+industry_esg = pd.read_sql_query(query, engine)
+
+# Sort the DataFrame
+industry_esg_sorted = industry_esg.sort_values(by='max_esg_score', ascending=False)
+max_esg_per_industry = industry_esg_sorted[['max_env_score', 'max_social_score', 'max_governance_score']].max().max()
+
+# Create a bar chart using Plotly
+industry_fig = go.Figure()
+
+industry_fig.add_trace(go.Bar(
+    x=industry_esg_sorted['industry'],
+    y=industry_esg_sorted['max_env_score'],
+    name='Environment',
+    marker_color='lightblue'
+))
+
+industry_fig.add_trace(go.Bar(
+    x=industry_esg_sorted['industry'],
+    y=industry_esg_sorted['max_social_score'],
+    name='Social',
+    marker_color='lightgreen'
+))
+
+industry_fig.add_trace(go.Bar(
+    x=industry_esg_sorted['industry'],
+    y=industry_esg_sorted['max_governance_score'],
+    name='Governance',
+    marker_color='lightpink'
+))
+
+# Update layout
+industry_fig.update_layout(
+    barmode='group',
+    xaxis_title='Industry',
+    yaxis_title='ESG Scores',
+    yaxis=dict(
+        title='ESG Scores',
+        range=[0, max_esg_per_industry + 100]  # Fixed y-axis range
+    ),
+    title='ESG Scores per Industry',
+    legend_title_text='Categories',
+    width=1200,  # Adjust width
+    height=800,  # Adjust height
+    legend=dict(yanchor="top", y=1.15, xanchor="left", x=1.05)
+)
+
+# Render the chart in Streamlit
+st.plotly_chart(industry_fig)
--- dashboard/home.py
+++ dashboard/home.py
@@ -1,9 +1,38 @@
 import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
 
 # Streamlit Title
 st.title("Sustainable Investing With ESG")
+
+engine = create_engine("postgresql://team13:team13@esg-stocks-db:5432/esg-stocks-database")
+
+# Join all 3 tables and select certain columns
+query = """
+SELECT 
+st.ticker_symbol, 
+st.name, 
+MAX(CAST(esg.total_score AS numeric)) AS max_esg_score,
+MAX(CAST(esg.environment_score AS numeric)) AS max_env_score,
+MAX(CAST(esg.social_score AS numeric)) AS max_social_score,
+MAX(CAST(esg.governance_score AS numeric)) AS max_governance_score
+FROM stock AS st
+INNER JOIN esg_history AS esg ON st.ticker_symbol = esg.ticker_symbol
+GROUP BY st.ticker_symbol, st.name
+"""
+
+# Use pandas to read the data
+df = pd.read_sql_query(query, engine)
+
+# Sort the DataFrame
+df_sorted = df.sort_values(by='max_esg_score', ascending=False)
+# Determine the maximum ESG score across all stocks
+avg_env_score = df_sorted['max_env_score'].mean()
+avg_soc_score = df_sorted['max_social_score'].mean()
+avg_gov_score = df_sorted['max_governance_score'].mean()
+
 st.subheader("""
-         ESG stands for environmental, social, and governance. An ESG rating is a measure of a company’s performance along the criteria explained below.
+         _ESG_ stands for _environmental_, _social_, and _governance_. An ESG rating is a measure of a company’s performance along the criteria explained below.
          """)
 
 st.write('----------------------------------------------------------------------------------')
@@ -11,29 +40,34 @@ st.write('----------------------------------------------------------------------
 
 col1, col2, col3 = st.columns(3, gap='medium', vertical_alignment='top')
 with col1:
-    st.subheader("Environment (E)")
+    st.subheader(":earth_africa: Environment (E)")
     st.write("""
-             Environmental criteria focuses on a company’s environmental stewardship. 
-             The environmental segment of ESG examines how a company uses natural resources and impacts the environment. 
-             Environmental factors are primarily concerned with climate change issues.""")
+             This aspect emphasizes a company's commitment to environmental stewardship, assessing how it utilizes natural resources and its impact on the environment. It primarily addresses issues related to climate change.""")
  
     st.image("assets/environment.jpg", use_column_width='auto')
+    st.subheader(f"Average Score: {avg_env_score}")
 
 with col2:
-    st.subheader("Social (S)")
+    st.subheader(":people_holding_hands: Social (S)")
     st.write("""
-             Social criteria focuses on a company's social responsibility. 
-             The social segment of ESG examines how a company manages its relationships with its workforce and other stakeholders in its community.
-             Social factors are primarily concerned with social consequences of business decisions.""")
+            This criteria centers on a company’s social responsibility, evaluating how it manages relationships with its employees and the community. It focuses on the social implications of business activities.""")
     
     st.image("assets/society.jpg", use_column_width='auto')
+    st.subheader(f"Average Score: {avg_soc_score}")
 
 with col3:
-    st.subheader("Governance (G)")
+    st.subheader(":classical_building: Governance (G)")
     st.write("""
-             Governance criteria focuses on the integrity of a company’s governance structure. 
-             The governance segment of ESG examines whether a company’s leadership is operating within an ethical and responsible framework. 
-             Governance factors are primarily concerned with leadership accountability.
+             Governance deals with the integrity of a company’s management structure, examining if leadership operates ethically and responsibly. It is mainly concerned with accountability within leadership.
              """)
     
     st.image("assets/governance.jpg", use_column_width='auto')
+
+    st.subheader(f"Average Score: {avg_gov_score}")
+
+
+st.write('----------------------------------------------------------------------------------')
+
+st.subheader("""
+         Here goes the text about Sustainable Investing
+         """)
